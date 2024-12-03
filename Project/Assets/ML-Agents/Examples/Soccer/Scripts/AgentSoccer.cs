15e3d15b757b429a4357bce06ec95853a310aed4
SirKryo <balagtas.cielocarl@gmail.com>
2024-12-03 20:26:22 +0100

    Revert "revert this to previous version after"
    This reverts commit 1fb71236fdcb7764ff0c9998c898f65e65d7b780.

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/AgentSoccer.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/AgentSoccer.cs
@@ -11,6 +11,14 @@ public enum Team
 
 public class AgentSoccer : Agent
 {
+    // Note that that the detectable tags are different for the blue and purple teams. The order is
+    // * ball
+    // * own goal
+    // * opposing goal
+    // * wall
+    // * own teammate
+    // * opposing player
+
     public enum Position
     {
         Striker,
@@ -21,6 +29,7 @@ public class AgentSoccer : Agent
     [HideInInspector]
     public Team team;
     float m_KickPower;
+    // The coefficient for the reward for colliding with a ball. Set using curriculum.
     float m_BallTouch;
     public Position position;
 
@@ -28,6 +37,7 @@ public class AgentSoccer : Agent
     float m_Existential;
     float m_LateralSpeed;
     float m_ForwardSpeed;
+
 
     [HideInInspector]
     public Rigidbody agentRb;
@@ -37,18 +47,6 @@ public class AgentSoccer : Agent
     public float rotSign;
 
     EnvironmentParameters m_ResetParams;
-
-    [Header("Vision Settings")]
-    public float visionRange = 10f;
-    public float visionAngle = 90f;
-    public float visionRotationSpeed = 45f;
-    private float currentVisionAngle = 0f;
-
-    [SerializeField]
-    private Transform visionConeTransform;
-
-    [SerializeField]
-    private LayerMask visionMask;
 
     public override void Initialize()
     {
@@ -63,9 +61,6 @@ public class AgentSoccer : Agent
         }
 
         m_BehaviorParameters = gameObject.GetComponent<BehaviorParameters>();
-        var discreteActionSpec = ActionSpec.MakeDiscrete(3, 3, 3, 3);
-        m_BehaviorParameters.BrainParameters.ActionSpec = discreteActionSpec;
-
         if (m_BehaviorParameters.TeamId == (int)Team.Blue)
         {
             team = Team.Blue;
@@ -98,12 +93,6 @@ public class AgentSoccer : Agent
         agentRb.maxAngularVelocity = 500;
 
         m_ResetParams = Academy.Instance.EnvironmentParameters;
-
-        if (visionConeTransform == null)
-        {
-            visionConeTransform = transform.Find("VisionCone");
-        }
-        currentVisionAngle = transform.eulerAngles.y;
     }
 
     public void MoveAgent(ActionSegment<int> act)
@@ -116,7 +105,6 @@ public class AgentSoccer : Agent
         var forwardAxis = act[0];
         var rightAxis = act[1];
         var rotateAxis = act[2];
-        var visionConeAxis = act[3];
 
         switch (forwardAxis)
         {
@@ -149,42 +137,23 @@ public class AgentSoccer : Agent
                 break;
         }
 
-        switch (visionConeAxis)
-        {
-            case 1:
-                AdjustVisionCone(1);
-                break;
-            case 2:
-                AdjustVisionCone(-1);
-                break;
-            default:
-                AdjustVisionCone(0);
-                break;
-        }
-
         transform.Rotate(rotateDir, Time.deltaTime * 100f);
         agentRb.AddForce(dirToGo * m_SoccerSettings.agentRunSpeed,
             ForceMode.VelocityChange);
     }
 
-    private void AdjustVisionCone(int direction)
-    {
-        currentVisionAngle += direction * visionRotationSpeed * Time.deltaTime;
-
-        if (visionConeTransform != null)
-        {
-            visionConeTransform.localRotation = Quaternion.Euler(0, currentVisionAngle, 0);
-        }
-    }
-
     public override void OnActionReceived(ActionBuffers actionBuffers)
+
     {
+
         if (position == Position.Goalie)
         {
+            // Existential bonus for Goalies.
             AddReward(m_Existential);
         }
         else if (position == Position.Striker)
         {
+            // Existential penalty for Strikers
             AddReward(-m_Existential);
         }
         MoveAgent(actionBuffers.DiscreteActions);
@@ -193,7 +162,7 @@ public class AgentSoccer : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var discreteActionsOut = actionsOut.DiscreteActions;
-
+        //forward
         if (Input.GetKey(KeyCode.W))
         {
             discreteActionsOut[0] = 1;
@@ -202,6 +171,7 @@ public class AgentSoccer : Agent
         {
             discreteActionsOut[0] = 2;
         }
+        //rotate
         if (Input.GetKey(KeyCode.A))
         {
             discreteActionsOut[2] = 1;
@@ -210,6 +180,7 @@ public class AgentSoccer : Agent
         {
             discreteActionsOut[2] = 2;
         }
+        //right
         if (Input.GetKey(KeyCode.E))
         {
             discreteActionsOut[1] = 1;
@@ -218,17 +189,10 @@ public class AgentSoccer : Agent
         {
             discreteActionsOut[1] = 2;
         }
-
-        if (Input.GetKey(KeyCode.Z))
-        {
-            discreteActionsOut[3] = 1;
-        }
-        if (Input.GetKey(KeyCode.C))
-        {
-            discreteActionsOut[3] = 2;
-        }
     }
-
+    /// <summary>
+    /// Used to provide a "kick" to the ball.
+    /// </summary>
     void OnCollisionEnter(Collision c)
     {
         var force = k_Power * m_KickPower;
@@ -249,4 +213,5 @@ public class AgentSoccer : Agent
     {
         m_BallTouch = m_ResetParams.GetWithDefault("ball_touch", 0);
     }
+
 }
